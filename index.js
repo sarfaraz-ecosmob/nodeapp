655660a788b8fb9777a2bf88edd10eb8e17a7d7b
ashishpandyaecm <ashish.pandya@ecosmob.com>
2024-11-14 22:48:05 +0530

    testing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello, This is demo page for Webinar" }');
+    res.send('{ "response": "Hello, This is demo page for Webinar v1" }');
 });
 
 app.get('/will', function (req, res) {
